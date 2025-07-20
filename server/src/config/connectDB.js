dfe866e6cccf2b80d4c48d370d6473f5af7d957e
Ctit28 <nguyenmap29082003@gmail.com>
2025-07-20 22:56:53 +0700

    update v1

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -1,21 +1,21 @@
-import mysql from "mysql2/promise";
-
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bookstore",
-});
-
-export default pool;
-
 // import mysql from "mysql2/promise";
 
 // const pool = mysql.createPool({
-//   host: "mysql.railway.internal",
+//   host: "localhost",
 //   user: "root",
-//   password: "rOLiyryUxnaWiGookJvPyYHqPobSUkLw",
-//   database: "railway",
+//   password: "",
+//   database: "bookstore",
 // });
 
 // export default pool;
+
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "sql.freedb.tech",
+  user: "freedb_booklover",
+  password: "4!NrjA6@EjkyYvH",
+  database: "freedb_bookstore1",
+});
+
+export default pool;
