d6bb2a03199fbe9affd2f53f6a17bc21938bbcd6
Ctit28 <nguyenmap29082003@gmail.com>
2025-07-20 21:51:52 +0700

    up last v8

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -1,10 +1,21 @@
+// import mysql from "mysql2/promise";
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "bookstore",
+// });
+
+// export default pool;
+
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "mysql.railway.internal",
   user: "root",
-  password: "",
-  database: "bookstore",
+  password: "rOLiyryUxnaWiGookJvPyYHqPobSUkLw",
+  database: "railway",
 });
 
 export default pool;
